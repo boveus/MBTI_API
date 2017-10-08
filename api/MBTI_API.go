44d05c74aafeab36c449b56c1ab7e949b962089c
Brandon Stewart <boveus@gmail.com>
2017-10-08 00:37:36 -0600

    Add function to query database and return a map of structs.

--- api/MBTI_API.go
+++ api/MBTI_API.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
-	"strconv"
+	"log"
 )
 
 var database *sql.DB
+var query_data []Data
+
+type Data struct {
+	mbti_type string
+	posts     string
+}
 
 func init() {
 	database, _ = sql.Open("sqlite3", "./data/mbti_data.db")
@@ -16,11 +21,15 @@ func main() {
 }
 
 func query_by_type(mbti_type string) {
+	query_data = nil
 	rows, _ := database.Query("SELECT mbti_type, posts FROM mbti_data WHERE mbti_type=?", mbti_type)
-	var id int
 	var posts string
 	for rows.Next() {
-		rows.Scan(&id, &mbti_type, &posts)
-		fmt.Println(strconv.Itoa(id) + ": " + mbti_type + " " + posts)
+		err := rows.Scan(&mbti_type, &posts)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		query_data = append(query_data, Data{mbti_type: mbti_type, posts: posts})
 	}
 }
