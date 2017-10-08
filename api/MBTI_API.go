0fd4cad7ccc686108051c413edaa00046a88cbe0
Brandon Stewart <boveus@gmail.com>
2017-10-07 23:06:11 -0600

    add a global variable for database and add it to the init function

--- api/MBTI_API.go
+++ api/MBTI_API.go
@@ -6,17 +6,21 @@ import (
 	"strconv"
 )
 
+var database *sql.DB
+
+func init() {
+	database, _ = sql.Open("sqlite3", "./data/mbti_data.db")
+}
+
 func main() {
+}
 
-	database, _ := sql.Open("sqlite3", "./data/mbti_data.db")
-
+func query_by_type(mbti_type string) {
 	rows, _ := database.Query("SELECT id, mbti_type, posts FROM mbti_data LIMIT 5")
 	var id int
-	var mbti_type string
 	var posts string
 	for rows.Next() {
 		rows.Scan(&id, &mbti_type, &posts)
 		fmt.Println(strconv.Itoa(id) + ": " + mbti_type + " " + posts)
 	}
-
 }
