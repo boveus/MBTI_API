2b2c1be5d63f6058c7b40522db10ad5e6ed2abba
Brandon Stewart <boveus@gmail.com>
2017-10-07 22:29:17 -0600

    Add initial with working database seeding file

---
+++ app/populate_database.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	database, _ := sql.Open("sqlite3", "./data/mbti_data.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS mbti_data (id INTEGER PRIMARY KEY, mbti_type TEXT, posts TEXT)")
+	statement.Exec()
+
+	f, _ := os.Open("./data/mbti_1.csv")
+
+	r := csv.NewReader(f)
+	r.Comma = ','
+	r.Comment = '#'
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		statement, _ = database.Prepare("INSERT INTO mbti_data (mbti_type, posts) VALUES (?, ?)")
+		statement.Exec(record[0], record[1])
+
+	}
+
+	rows, _ := database.Query("SELECT id, mbti_type, posts FROM mbti_data LIMIT 5")
+	var id int
+	var mbti_type string
+	var posts string
+	for rows.Next() {
+		rows.Scan(&id, &mbti_type, &posts)
+		fmt.Println(strconv.Itoa(id) + ": " + mbti_type + " " + posts)
+	}
+
+}
