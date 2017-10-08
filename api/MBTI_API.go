54837fdab362b8e8f3903d62ce66b66ce273dc59
Brandon Stewart <boveus@gmail.com>
2017-10-08 11:39:43 -0600

    Add working (non-secure) route for JSON

--- api/MBTI_API.go
+++ api/MBTI_API.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"encoding/json"
 	"log"
+	"net/http"
 
 	sqlite3 "github.com/mattn/go-sqlite3"
 )
@@ -12,8 +13,8 @@ var database *sql.DB
 var query_data []Data
 
 type Data struct {
-	mbti_type string
-	posts     string
+	MBTI_type string `json:"MBTI_type"`
+	Posts     string `json:"Posts"`
 }
 
 func init() {
@@ -22,19 +23,28 @@ func init() {
 }
 
 func main() {
-	query_by_type("INTP")
-	fmt.Printf("%v", query_data)
+	http.HandleFunc("/MBTI", func(w http.ResponseWriter, r *http.Request) {
+		mbti_type := r.URL.Query().Get("type")
+		if mbti_type != "" {
+			query_by_type(mbti_type)
+			query_data, _ := json.Marshal(query_data)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(query_data)
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
 func query_by_type(mbti_type string) {
 	query_data = nil
-	rows, _ := database.Query("SELECT mbti_type, posts FROM mbti_data WHERE mbti_type=? LIMIT 5", mbti_type)
+	rows, _ := database.Query("SELECT mbti_type, posts FROM mbti_data WHERE mbti_type=?", mbti_type)
 	var posts string
 	for rows.Next() {
 		err := rows.Scan(&mbti_type, &posts)
 		if err != nil {
 			log.Fatal(err)
 		}
-		query_data = append(query_data, Data{mbti_type: mbti_type, posts: posts})
+		query_data = append(query_data, Data{MBTI_type: mbti_type, Posts: posts})
 	}
 }
